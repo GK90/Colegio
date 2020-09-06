8b4dd912fce3a26ca9437d431b5dec2d15b24084
GeovanyFranco <vfrancos@miumg.edu.gt>
2020-09-06 17:20:28 -0600

    Agregado template de bootstrap Sigue conectarlo a la base de datos

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <!--estilo-->
+        <link rel="stylesheet" href="recursos/css/simple-sidebar.css" type="text/css">
+        <link href="recursos/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
+
+            <!-- /#sidebar-wrapper -->
+            <?php require './partes/NavIsquierdo.php'; ?>
+
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <button class="btn btn-primary" id="menu-toggle">Alternar menu</button>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                            </li>
+                            <!-- 
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Opciones
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Buscar</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Agregar</a>
+                                    <a class="dropdown-item" href="#">Modificar</a>
+                                    <a class="dropdown-item" href="#">Eliminar</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div class="container-fluid">
+                    <iframe src="partes/NavIsquierdo.php" style="width: 90%; height: 80%" name="formularios"></iframe>
+                    
+                    <h1 class="mt-4">Simple Sidebar</h1>
+                    <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+                    <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
+                </div>
+            </div>
+            <!-- /#page-content-wrapper -->
+
+        </div>
+        <!-- /#wrapper -->
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="recursos/jquery/jquery.min.js"></script>
+        <script src="recursos/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Menu Toggle Script -->
+        <script>
+            $("#menu-toggle").click(function (e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+        </script>
+    </body>
+</html>
---
+++ partes/NavIsquierdo.php
@@ -0,0 +1,13 @@
+<!-- Sidebar -->
+<div class="bg-light border-right" id="sidebar-wrapper">
+    <div class="sidebar-heading">Control academico</div>
+    <div class="list-group list-group-flush">
+        <a href="#" class="list-group-item list-group-item-action bg-light">Inicio</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Alumnos</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Departamento</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Cursos</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Notas</a>
+    </div>
+</div>
+<!-- /#sidebar-wrapper -->
+
